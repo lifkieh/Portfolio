ebb29dae02b660e9642d606769e66861291b5ef3
Lifkie Lie <llifkie@gmail.com>
2026-05-15 14:29:01 +0700

    feat: add projects page and include resume asset

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -1,8 +1,8 @@
 import ProjectCard from '../../components/ProjectCard'
 import { getProjects } from '../../data/projects' 
 
-export default function ProjectsPage() {
-  const projects = getProjects()
+export default async function ProjectsPage() {
+  const projects = await getProjects()
   return (
     <div className="min-h-screen bg-[#120b18] text-[#f1eaf5]">
       <div className="max-w-6xl mx-auto px-6 py-12">
